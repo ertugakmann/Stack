ed2d18a42f19c7a44fa6103cae1eb14d2fcf816b
ertugakmann <134059022+ertugakmann@users.noreply.github.com>
2024-07-16 21:11:04 +0300

    Proje dosyası ekle.

---
+++ Stack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Stack
+    {
+        private Node top;
+        private int count;
+
+        public Stack()
+        {
+            top = null;
+            count = 0;
+        }
+
+        public Stack(int data)
+        {
+            Node newNode = new Node(data);
+            top = newNode;
+            count = 1;
+        }
+
+        public void Push(int data)
+        {
+            Node newNode = new Node(data);
+
+            if(count == 0)
+            {
+                newNode = top;
+            }
+            else
+            {
+                newNode.next = top;
+                top = newNode;
+                newNode = top;
+            }
+            count++;
+        }
+    }
+}
